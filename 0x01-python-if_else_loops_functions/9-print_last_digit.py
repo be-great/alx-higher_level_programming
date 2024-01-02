9c2478835748dbde6e0d766ab3731ac870e5772c
be-great <begreat5333@gmail.com>
2024-01-02 10:55:15 -0800

    update Task

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@ def print_last_digit(number):
     if number >= 0:
         digit = number % 10
     else:
-        digit = number % -10
+        digit = -1 * (number % -10)
     print("{}".format(digit), end='')
     return digit
