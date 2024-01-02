1cb541e8c00301eb481f94258423298b833a881d
be-great <begreat5333@gmail.com>
2024-01-02 10:10:44 -0800

    task

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(100):
     if x < 99:
-        print("%02d"%(x), end=", ")
+        print("{:02d}".format(x), end=", ")
     else:
-        print("%02d"%(x))
+        print("{:02d}".format(x))
