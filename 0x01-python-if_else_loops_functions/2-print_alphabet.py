8fe72ab30dfc683b217bd90679f71864211bfbd4
be-great <begreat5333@gmail.com>
2024-01-02 06:44:43 -0800

    Task 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    if c == 101 or c == 113:
-        continue
-    print('{:c}'.format(c), end='')
+    if c != 101 and c != 113:
+        print('{:c}'.format(c), end='')
