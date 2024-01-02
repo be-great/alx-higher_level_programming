52c52cd39dc6b45fc6bd14b768914f3f2965f3f0
be-great <begreat5333@gmail.com>
2024-01-02 11:07:49 -0800

    Task 1

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1, 101):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz", end='')
+        elif x % 3 == 0:
+            print("Fizz", end='')
+        elif x % 5 == 0:
+            print("Buzz", end='')
+        else:
+            print("{}".format(x), end='')
+        if x < 100:
+            print(" ", end='')
+        else:
+            print()
+fizzbuzz()
