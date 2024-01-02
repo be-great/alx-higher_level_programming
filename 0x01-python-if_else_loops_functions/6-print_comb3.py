cb8a6e9c4e94a8a1aeaa16c41d7da579c1148d22
be-great <begreat5333@gmail.com>
2024-01-02 10:14:28 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for x in range(10):
     for y in range(x + 1, 10):
-        print(f"{x}{y}", end="")
+        print("{}{}".format(x, y), end="")
         if (x + y != 17):
             print(", ", end="")
 print("")
