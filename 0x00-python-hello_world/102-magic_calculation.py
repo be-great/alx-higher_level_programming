89604c3bd4fc72a3618ce0efec5cfbf56cf4ef8d
be-great <begreat5333@gmail.com>
2024-01-03 19:47:54 +0300

    task 13

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -4,11 +4,11 @@ import dis
 3           0 LOAD_CONST   1 (98) # load const to the stack
 3           3 LOAD_FAST    0 (a)  # load variable name a to stack
 3           6 LOAD_FAST    1 (b)  # load variable name b to stack
-3           9 BINARY_POWER  # rise the b**a (last on the stack)
-3          10 BINARY_ADD    # add result of previous b**a + 98
+3           9 BINARY_POWER  # rise the a**b (last on the stack)
+3          10 BINARY_ADD    # add result of previous 98 + a**b
 3          11 RETURN_VALUE  # return the last operation
 """
 
 
 def magic_calculation(a, b):
-    return 98 + b ** a
+    return 98 + a ** b
