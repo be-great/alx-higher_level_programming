b55488c71f98543601ef717d00388aa0594c1ae6
be-great <begreat5333@gmail.com>
2024-01-03 22:41:14 +0300

    task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+""" Using slicing:-
+    my_string = my_string[:n] + my_string[m+1:]
+"""
+
+
+def remove_char_at(str, n):
+    # 1- check if position is right
+    if n >= len(str):
+        return str
+    # 2- check if position is negative
+    if n < 0:
+        n = len(str) - n
+    # slice it
+    str = str[:n] + str[n+1:]
+    return str
