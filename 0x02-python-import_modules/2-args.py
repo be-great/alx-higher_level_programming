6acbce04f386b4f6cc24127b3b19f963865b970d
be-great <begreat5333@gmail.com>
2024-01-04 18:03:34 +0300

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,5 +5,5 @@ if length == 1:
     print("{} arguments.".format(len(sys.argv) - 1))
 else:
     print("{} arguments:".format(len(sys.argv) - 1))
-for i, arg in enumerate(sys.argv[1:]):
-    print("{}: {}".format(i + 1, arg))
+for x in range(1, length):
+    print("{}: {}".format(x, sys.argv[x]))
