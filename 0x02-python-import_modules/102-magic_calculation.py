ed6b50056a3eb68daad94ea9aa88a29c25879935
be-great <begreat5333@gmail.com>
2024-01-05 04:06:06 +0300

    update magic task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
-            c = add(4, 6)
+            c = add(c, i)
         return c
     else:
         return sub(a, b)
