466e137a3b996e8adb46d5d2f157f86dd3cc0c91
be-great <begreat5333@gmail.com>
2024-01-05 04:04:45 +0300

    Task magic

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(4, 6)
+        return c
+    else:
+        return sub(a, b)
