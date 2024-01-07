21c2acb7deb8f01e8a8a42bcb7dbf99d2a0d4a33
be-great <begreat5333@gmail.com>
2024-01-07 22:38:54 +0300

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
-#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    if sentence:
+        b = len(sentence), sentence[0]
+        return b
+    else:
+        b = len(sentence), None
+        return b
