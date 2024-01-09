ceb625a6e2a71c879f50bd0ff385b5cdad90bc6e
be-great <begreat5333@gmail.com>
2024-01-09 05:23:25 +0300

    task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -15,7 +15,7 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Allocated = %li\n", pyobj->allocated);
 	for (i = 0; i < len; i++)
 	{
-		str = Py_TYPE(pyobj->ob_item[i])->tp_name
+		str = Py_TYPE(pyobj->ob_item[i])->tp_name;
 		printf("Element %i: %s\n", i, str);
 	}
 }
