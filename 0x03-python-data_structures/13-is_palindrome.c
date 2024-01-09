4b97530c2da07096ba38fc6a56fc0ae4d87518fa
be-great <begreat5333@gmail.com>
2024-01-09 03:23:30 +0300

    update task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,11 +1,31 @@
 #include "lists.h"
 
 /**
- * reverse_listint - Reverses a linked list.
- * @head: Double pointer to the head of the linked list.
- * Description: This function reverses a linked list and updates the head.
- */
-void reverse_listint(listint_t **head)
+* find_second_head - Finds the second half of the linked list.
+* @head: Pointer to the head of the linked list.
+* Return: Pointer to the second half of the list.
+*/
+listint_t *find_second_head(listint_t *head)
+{
+	listint_t *slow = head, *fast = head;
+
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+			return (slow->next);
+		if (!fast->next)
+			return (slow->next->next);
+		slow = slow->next;
+	}
+}
+
+/**
+* reverse_list - Reverses a linked list.
+* @head: Double pointer to the head of the linked list.
+* Description: This function reverses a linked list and updates the head.
+*/
+void reverse_list(listint_t **head)
 {
 	listint_t *prev = NULL, *current = *head, *next;
 
@@ -21,69 +41,32 @@ void reverse_listint(listint_t **head)
 }
 
 /**
- * compare_lists - Compares two linked lists for equality.
- * @list1: Pointer to the first linked list.
- * @list2: Pointer to the second linked list.
- * Return: 1 if lists are equal, 0 otherwise.
- * Description: This function compares two linked lists node by node.
- */
-int compare_lists(listint_t *list1, listint_t *list2)
-{
-	while (list1 != NULL && list2 != NULL)
-	{
-		if (list1->n != list2->n)
-			return (0);
-
-		list1 = list1->next;
-		list2 = list2->next;
-	}
-
-	return (1);
-}
-
-/**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Double pointer to the head of the linked list.
- * Return: 1 if it's true, 0 if it's false.
- * Description: This function checks if a linked list is a palindrome.
- */
+* is_palindrome - Checks if a singly linked list is a palindrome.
+* @head: Double pointer to the head of the linked list.
+* Return: 1 if it's true, 0 if it's false.
+* Description: This function checks if a linked list is a palindrome.
+*/
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
-
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	while (1)
-	{
-		fast = fast->next->next;
-		if (!fast)
-		{
-			dup = slow->next;
-			break;
-		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
-	}
+	listint_t *second_head = find_second_head(*head);
 
-	reverse_listint(&dup);
+	reverse_list(&second_head);
 
-	while (dup && temp)
+	while (second_head && *head)
 	{
-		if (temp->n == dup->n)
+		if ((*head)->n == second_head->n)
 		{
-			dup = dup->next;
-			temp = temp->next;
+			second_head = second_head->next;
+			*head = (*head)->next;
 		}
 		else
 			return (0);
 	}
 
-	if (!dup)
+	if (!second_head)
 		return (1);
 
 	return (0);
