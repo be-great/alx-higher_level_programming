5a913e4a38d71445af29585c134e885ad05add6a
be-great <begreat5333@gmail.com>
2024-01-10 19:08:37 +0300

    task 13-15

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    total_score = sum(sore * weight for sore, weight in my_list)
+    total_weight = sum(weigth for score, weigth in my_list)
+
+    return total_score / total_weight
--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,1 +1,3 @@
 #!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    return [a_dictionary.pop(key) for key, val in a_dictionary.items() if val == value ]
