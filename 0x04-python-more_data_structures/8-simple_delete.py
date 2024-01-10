2213cc935c2a872d4fe9531b2c2354e30b24dd2b
be-great <begreat5333@gmail.com>
2024-01-11 00:22:25 +0300

    task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -6,3 +6,5 @@ def simple_delete(a_dictionary, key=""):
     # check if key exist or not
     if key in a_dictionary:
         del a_dictionary[key]
+    new_dic = a_dictionary.copy()
+    return new_dic
