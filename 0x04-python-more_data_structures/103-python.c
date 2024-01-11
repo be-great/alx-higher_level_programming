2ee07a786f95350120fb4359db73d3ab79be9e7a
be-great <begreat5333@gmail.com>
2024-01-11 05:15:35 +0300

    update task -1

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,14 +2,15 @@
 #include <Python.h>
 
 /**
-* print_python_bytes - Prints bytes object information
+* print_python_bytes - functions that print some basic info about
+* python bytes object
+* discription: check if python object is type sbytes (string in python)
+* varaible : lable name of the object , value (object value)
 * @p: Python bytes object
-* Return: No return
 */
 void print_python_bytes(PyObject *p)
 {
 	char *byte_string;
-
 	long int size, i;
 	long int limit;
 
@@ -21,12 +22,14 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
-	size = ((PyVarObject *)(p))->ob_size;
-	byte_string = ((PyBytesObject *)p)->ob_sval;
-	limit = (size >= 10) ? 10 : size + 1;
+	size = PyBytes_Size(p);
+	byte_string = PyBytes_AsString(p);
 
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", byte_string);
+
+	limit = (size >= 10) ? 10 : size + 1;
+
 	printf("  first %ld bytes:", limit);
 
 	for (i = 0; i < limit; i++)
@@ -36,18 +39,19 @@ void print_python_bytes(PyObject *p)
 }
 
 /**
-* print_python_list - Prints list information
-*
+* print_python_list - functions that print some basic info about
+* python list object
 * @p: Python list object
 * Return: No return
 */
 void print_python_list(PyObject *p)
 {
 	long int size, i;
+
 	PyListObject *list;
 	PyObject *list_item;
 
-	size = ((PyVarObject *)(p))->ob_size;
+	size = PyList_Size(p);
 	list = (PyListObject *)p;
 
 	printf("[*] Python list info\n");
@@ -58,6 +62,7 @@ void print_python_list(PyObject *p)
 	{
 		list_item = PyList_GetItem(p, i);
 		printf("Element %ld: %s\n", i, Py_TYPE(list_item)->tp_name);
+
 		if (PyBytes_Check(list_item))
 			print_python_bytes(list_item);
 	}
