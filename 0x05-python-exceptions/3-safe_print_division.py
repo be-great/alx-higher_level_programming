601603092e1b1d1b660f248ca9f3a1c33e289c20
be-great <begreat5333@gmail.com>
2024-01-23 00:37:21 +0300

    task 3 update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-    except ZeroDivisionError:
+    except (TypeError, ZeroDivisionError):
         return None
     finally:
         print("Inside result: {}".format(result))
