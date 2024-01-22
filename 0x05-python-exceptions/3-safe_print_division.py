82bf25318bd75469dade9678675ac66523027e7d
be-great <begreat5333@gmail.com>
2024-01-23 00:36:10 +0300

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
+    """a function that divides 2 integers and prints the result.
+    Prototype"""
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
