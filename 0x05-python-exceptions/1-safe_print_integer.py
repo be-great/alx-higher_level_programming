340fea48d2e29540565b50a88f3c80ed165cf27a
be-great <begreat5333@gmail.com>
2024-01-22 22:53:31 +0300

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    
+    status = None
+    try:
+        print("{:d}".format(value))
+        status = True
+    except ValueError:
+        status = False
+    return status
