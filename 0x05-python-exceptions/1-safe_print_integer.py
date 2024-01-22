e5ba74f0727d900f67a18797f1dcdb33bd0fe8c0
be-great <begreat5333@gmail.com>
2024-01-23 00:41:37 +0300

    test 1, 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return True
+        return (True)
     except ValueError:
-        return False
+        return (False)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,11 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     length = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             length += 1
         except (ValueError, IndexError):
             continue
     print()
-    return length
+    return (length)
