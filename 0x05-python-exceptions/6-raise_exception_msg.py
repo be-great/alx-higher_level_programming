769a6f8baaa665880a32954fb06f782589e73874
be-great <begreat5333@gmail.com>
2024-01-23 01:01:23 +0300

    task 6

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
 def raise_exception_msg(message=""):
+    """function that raises a name exception with a message."""
+    try:
+        # use undefined variable
+        print(x)
+    except NameError as e:
+        raise NameError(message) from e
