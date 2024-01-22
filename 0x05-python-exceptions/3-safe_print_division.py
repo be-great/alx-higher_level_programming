9468afe98a0ed5fd155fa9ec97329c2e4475243f
be-great <begreat5333@gmail.com>
2024-01-23 00:38:17 +0300

    Task 3 udpate

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except (TypeError, ZeroDivisionError):
-        return None
+        result = None
     finally:
         print("Inside result: {}".format(result))
     return result
