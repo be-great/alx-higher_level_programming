dda798adcae7d565bbb1fbc54b78c089524e5200
be-great <begreat5333@gmail.com>
2024-01-23 00:55:18 +0300

    Task 5

--- 0x05-python-exceptions/5-main.py
+++ 0x05-python-exceptions/5-main.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 raise_exception = __import__('5-raise_exception').raise_exception
 
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 
 def raise_exception():
+    """function that raises a type exception."""
+    raise TypeError
