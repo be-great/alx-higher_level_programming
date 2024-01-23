49502d957a7788837a0ca181b4c49832190d7a45
be-great <begreat5333@gmail.com>
2024-01-24 00:28:19 +0300

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""this is a program that have a class with private attribute"""
+
+
+class Square:
+    """a class have:
+    size : Private instance attribute
+    with exception handling
+    """
+    def __init__(self, size=0):
+        """a private instance attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+
+        return self.__size ** 2
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
