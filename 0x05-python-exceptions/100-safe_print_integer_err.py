2588c68f19cda7bad3db1736202051b817c9ac44
be-great <begreat5333@gmail.com>
2024-01-23 03:21:21 +0300

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,3 +4,10 @@ import sys
 
 
 def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        x = sys.exc_info()[1]
+        print("Exception: {}".format(x), file=sys.stderr)
+        return (False)
