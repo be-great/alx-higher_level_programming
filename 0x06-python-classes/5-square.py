040d1aeb90633c7563215d073a980a464faec510
be-great <begreat5333@gmail.com>
2024-01-24 00:35:18 +0300

    Task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+"""this is a program that have a class with private attribute"""
+
+
+class Square:
+    """a class have:
+    size : Private instance attribute
+    with exception handling
+    """
+    def __init__(self, size=0):
+        """a private instance attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """get the size instance attribute"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set the size instance attribute"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+
+        size = self.__size
+        if self.__size <= 0:
+            print()
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print()
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -30,7 +30,7 @@
 |2-square.py| Private instance attribute: size, Instantiation with optional size: def __init__(self, size=0). size must be an integer, otherwise raise a TypeError exception with the message size must be an integer , if size is less than 0, raise a ValueError exception with the message size must be >= 0|
 |3-square.py|add Public instance method: def area(self): that returns the current square area|
 |4-square.py|property def size(self): to retrieve it , property setter def size(self, value): to set it|
-|5-square.py||
+|5-square.py|Public instance method: def my_print(self): that prints in stdout the square with the character #|
 |6-square.py||
 |100-singly_linked_list.py||
 |101-square.py||
