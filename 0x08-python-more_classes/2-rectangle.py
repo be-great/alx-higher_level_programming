5b6e9c0814d7b859ddeb43226c6d8f469d5ac1e1
be-great <begreat5333@gmail.com>
2024-01-29 15:33:47 +0300

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,1 +1,56 @@
 #!/usr/bin/python3
+"""this is a program that have a class with private attribute"""
+
+
+class Rectangle:
+    """a class have:
+    size : Private instance attribute
+    with exception handling
+    """
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """get the width instance attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the size instance attribute"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get the width instance attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set the size instance attribute"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return (self.__width + self.__height) * 2
