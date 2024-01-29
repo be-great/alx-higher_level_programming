1971bed56c73b7e9d5d828ec3006456eb6f0c289
be-great <begreat5333@gmail.com>
2024-01-29 15:30:34 +0300

    task 1 update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,6 +8,14 @@ class Rectangle:
     with exception handling
     """
     def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__height = height
         self.__width = width
 
