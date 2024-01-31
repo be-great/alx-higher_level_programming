9583890ca65d84f68825ff2a394635480820d1de
be-great <begreat5333@gmail.com>
2024-02-01 00:10:59 +0300

    3-say-name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,4 +15,5 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    if len(first_name) != 0 or len(last_name) != 0:
+        print("My name is {} {}".format(first_name, last_name))
---
+++ 0x07-python-test_driven_development/tests/3-maic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+say_my_name = __import__('3-say_my_name').say_my_name
+
+say_my_name("John", "Smith")
+say_my_name("Walter", "White")
+say_my_name("Bob")
+try:
+    say_my_name(12, "White")
+except Exception as e:
+    print(e)
+try:
+    say_my_name("", "")
+except Exception as e:
+    print(e)
