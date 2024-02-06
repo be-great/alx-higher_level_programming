37309322f7d98b233c5aaa7ef089465937fde8eb
be-great <begreat5333@gmail.com>
2024-02-06 22:28:11 +0300

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""
 
 """
+function function that returns an object
+representation of an object (string)
+"""
+import json
+
+
 def from_json_string(my_str):
     """
-    
+    args : (str) my_str
     """
-    pass
+    return json.loads(my_str)
