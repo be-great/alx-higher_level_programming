2985dfa5e8846f05f79a211ff583c0790c720054
be-great <begreat5333@gmail.com>
2024-02-06 22:43:30 +0300

    task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """
-
+function that creates an Object from a “JSON file”
 """
+
+import json
+
 
 def load_from_json_file(filename):
     """
+    args: (str) filename
     """
-    pass
+    with open(filename, 'r') as file:
+        return json.load(file)
