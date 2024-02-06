e0b7286a677e6427f3d261b85aa066daf05adeb2
be-great <begreat5333@gmail.com>
2024-02-06 03:28:05 +0300

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+
+"""
+class Rectangle
+that inherits from BaseGeometry (7-base_geometry.py).
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Inherite from teh BaseGemoetry
+    """
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """implementing the area method"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        "return : [Rectangle] <width>/<height> "
+        string = "[Rectangle] " + str(self.__width)
+        string += "/" + str(self.__height)
+        return string
