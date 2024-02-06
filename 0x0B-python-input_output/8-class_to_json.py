2769d80bf2fbe15139a72e1b2d5459a5d4e202f3
be-great <begreat5333@gmail.com>
2024-02-06 23:49:39 +0300

    task  8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-
+function that returns the dictionary description with simple data structure
+(list, dictionary, string, integer and boolean)
+for JSON serialization of an object
 """
 
 
 def class_to_json(obj):
     """
-    
+    args: (instance) obj
     """
+    return obj.__dict__
