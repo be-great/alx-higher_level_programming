088b35b260a1231ab92943cb54cc61a6790eaeeb
be-great <begreat5333@gmail.com>
2024-02-06 22:24:26 +0300

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""
+import json
 
 """
+function that returns the JSON
+representation of an object (string)
+"""
+
 
 def to_json_string(my_obj):
     """
-    
+    args : (str)
     """
-    pass
+    return json.dumps(my_obj)
