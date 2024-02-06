1d904c6fc3d37a60d898a45ae8b93afbb24ccc0d
be-great <begreat5333@gmail.com>
2024-02-07 00:07:59 +0300

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
-
+"""
+class Student that defines a student by
 """
 
-"""
 
 class Student:
     """
+    args : first_name, last_name and age
     """
-    pass
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        If attrs is a list of strings, only attribute
+        names contained in this list must be retrieved
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key)
+                    for key in attrs if hasattr(self, key)}
+
+    def reload_from_json(self, json_data):
+        """replaces all attributes of the Student instance"""
+        for key, value in json_data.items():
+            setattr(self, key, value)
