bec37db1203c6cdfe573ba53aac75396f3634997
be-great <begreat5333@gmail.com>
2024-02-07 00:02:42 +0300

    task 9

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-
+"""
+class Student that defines a student by
 """
 
-"""
 
 class Student:
     """
+    args : first_name, last_name and age
     """
-    pass
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
