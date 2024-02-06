001d936c1bfcda65d0bf4ea23af7a9727a89a775
be-great <begreat5333@gmail.com>
2024-02-06 03:58:02 +0300

    task 101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+function that adds a new attribute to an object if it’s possible
+"""
+
+
+def add_attribute(class_name, attribute, value):
+    """
+    args: (class)     class_name
+          (var)       attribute
+          (any_type)  value
+    """
+    if not hasattr(class_name, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(class_name, attribute, value)
