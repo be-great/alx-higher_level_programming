fb2c6301f05a2108f81768ab9b3596e15f2e400d
be-great <begreat5333@gmail.com>
2024-02-07 00:24:10 +0300

    task 100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -19,4 +19,4 @@ def append_after(filename="", search_string="", new_string=""):
         for line in lines:
             file.write(line)
             if search_string in line:
-                file.write(new_string + '\n')
+                file.write(new_string)
