3b17370deb684ce0da856e4e3a4c709ca803e1fd
be-great <begreat5333@gmail.com>
2024-02-12 18:31:20 +0300

    task 15 updatee

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -23,6 +23,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries """
         if list_dictionaries is None or list_dictionaries == []:
