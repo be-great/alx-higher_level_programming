aadd3d0138baf67036c9ff6337609c5a05241ccb
be-great <begreat5333@gmail.com>
2024-05-21 14:48:28 -0700

    task 3

---
+++ 0x0D-SQL_introduction/temp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global sendemail.smtpserver "smtp.gmail.com"
+git config --global sendemail.smtpserverport "587"
+git config --global sendemail.smtpencryption "tls"
+git config --global sendemail.smtpuser "begreat5333@gmail.com"
+git config --global sendemail.smtppassword "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"
+
