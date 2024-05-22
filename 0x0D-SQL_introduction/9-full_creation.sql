2e4b27ec932da941d343ad0afcf96f547aa337e3
be-great <begreat5333@gmail.com>
2024-05-22 03:18:01 +0300

    update 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,10 @@
 -- full creation
-CREATE TABLE IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
     score INT
 );
-INSERT INTO first_table (id, name, score) VALUES (1, "John", 10);
-INSERT INTO first_table (id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO first_table (id, name, score) VALUES (3, "Bob", 14);
-INSERT INTO first_table (id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
