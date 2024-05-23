095824b83b172fa381e8b901638dc6fcf7c6dc7f
be-great <begreat5333@gmail.com>
2024-05-22 17:15:19 -0700

    init files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create user
-CREATE USER if not exists 'user_0d_1'@'localhost';
+CREATE USER if not exists 'user_0d_1'@'localhost' identified by 'user_0d_1_pwd';
 GRANT ALL privileges on *.* To 'user_0d_1'@'localhost';
 
