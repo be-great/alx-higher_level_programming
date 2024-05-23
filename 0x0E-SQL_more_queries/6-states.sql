cfc07d1daac995a5e4d653ad9911182eb5ba2013
be-great <begreat5333@gmail.com>
2024-05-22 17:44:54 -0700

    init file

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- create table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
 	id INT unique AUTO_INCREMENT NOT NULL,
 	name VARCHAR(256) NOT NULL
