6166cd8c31fb68df7abc0da81c9474ef55428ffe
be-great <begreat5333@gmail.com>
2024-05-22 19:38:46 -0700

    add

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,4 +1,3 @@
--- lists all shows contained in database
-SELECT tv_shows.title - tv_show_genres.genre_id from tv_shows LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title asc, tv_show_genres.genre_id asc;
+-- lists all shows 
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC,  tv_show_genres.genre_id;
