e0ec745d9ce13efbbe92a4f24ed8296a45aa4758
be-great <begreat5333@gmail.com>
2024-05-22 18:13:07 -0700

    init file

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- lists all cities
-SELECT id, name from cities where state_id in ( select id from states where name = 'California')
-order by id ASC;
+SELECT id, name from cities where state_id = ( select id from states where name = 'California') order by id ASC;
