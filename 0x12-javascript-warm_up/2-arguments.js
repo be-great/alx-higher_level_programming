b96e42df72d7a04ccac7978bb98385ceb27a3b57
be-great <begreat5333@gmail.com>
2024-06-18 20:57:39 +0300

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const len = process.argv.length;
+if (len === 2) {
+  console.log('No argument');
+} else if (len === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
