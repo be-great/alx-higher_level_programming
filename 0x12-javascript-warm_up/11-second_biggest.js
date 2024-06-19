761fd1405f6ef505a797864d25cabdf4bc368dd9
be-great <begreat5333@gmail.com>
2024-06-19 16:36:35 +0300

    update

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,30 +1,24 @@
 #!/usr/bin/node
 /* make the none number NaN */
 
-function findSecondBiggest()
-{
-    const args = process.argv.slice(2).map(Number);
+function findSecondBiggest () {
+  const args = process.argv.slice(2).map(Number);
 
-    if (args.length <= 1)
-    {
-        console.log(0);
-        return;
-    }
-    let max = -Infinity;
-    let secondmax = -Infinity;
+  if (args.length <= 1) {
+    console.log(0);
+    return;
+  }
+  let max = -Infinity;
+  let secondmax = -Infinity;
 
-    for (const x of args)
-    {
-        if (x > max)
-        {
-            secondmax = max;
-            max = x;
-        }
-        else if (x > secondmax && x < max) 
-        {
-            secondmax = x;
-        }
+  for (const x of args) {
+    if (x > max) {
+      secondmax = max;
+      max = x;
+    } else if (x > secondmax && x < max) {
+      secondmax = x;
     }
-    console.log(secondmax);
+  }
+  console.log(secondmax);
 }
 findSecondBiggest();
--- 0x12-javascript-warm_up/13-main.js
+++ 0x12-javascript-warm_up/13-main.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
 const add = require('./13-add').add;
 console.log(add(3, 5));
-
