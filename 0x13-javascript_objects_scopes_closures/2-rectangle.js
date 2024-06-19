7fb1410ee9498a8cce94609465520f64786f290c
be-great <begreat5333@gmail.com>
2024-06-19 18:57:05 +0300

    update

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (typeof w !== 'number' || typeof h !== 'number' ||
-                w <= 0 || h <= 0) {
-      this.width = undefined;
-      this.height = undefined;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
