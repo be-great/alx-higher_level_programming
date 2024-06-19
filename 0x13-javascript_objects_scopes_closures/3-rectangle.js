b8c31e1a9a514dba9241cc04b2cf56d8892a3559
be-great <begreat5333@gmail.com>
2024-06-19 19:47:27 +0300

    update

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+/* module.exports => object in nodejs to export the models */
+/* to used on other files */
+module.exports = Rectangle;
