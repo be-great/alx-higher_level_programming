3bbb1a4c7d2adddec96ce4ce757ffe4d29b2c38f
be-great <begreat5333@gmail.com>
2024-06-19 22:58:53 +0300

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const dict = require('./101-data').dict;
+const res = {};
+for (const id in dict) {
+  const occr = dict[id];
+  /* push to existing array or create new */
+  if (res[occr]) {
+    res[occr].push(id.toString());
+  } else {
+    res[occr] = [id.toString()];
+  }
+}
+console.log(res);
