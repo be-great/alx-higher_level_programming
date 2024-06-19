1e03f8afd17c414956b9a42f97848debdbecfe05
be-great <begreat5333@gmail.com>
2024-06-19 22:29:29 +0300

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+const list = require('./100-data.js').list;
+/* create a new list */
+const newlist = list.map((value, index) => value * index);
+console.log(list);
+console.log(newlist);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const fs = require('fs');
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+const out = process.argv[4];
+const f1 = fs.readFileSync(arg1).toString();
+const f2 = fs.readFileSync(arg2).toString();
+fs.writeFileSync(out, f1 + f2);
