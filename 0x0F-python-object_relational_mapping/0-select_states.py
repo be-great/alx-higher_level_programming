6a0933e31b0242363067d02920263b8320e776f0
be-great <begreat5333@gmail.com>
2024-06-21 23:54:53 +0300

    task 2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,10 @@
+"""script that lists all states from the database"""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
+    
     conn = MySQLdb.connect(host = "localhost", port = 3306, user = sys.argv[1],
                             passwd = sys.argv[2], db = sys.argv[3], charset="utf8")
     cur = conn.cursor()
