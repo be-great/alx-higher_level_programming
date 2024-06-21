434152d1606a6417ba883a41be6fc72a21c8ee60
be-great <begreat5333@gmail.com>
2024-06-22 01:07:14 +0300

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 """script that lists all states from the database"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host = "localhost", port = 3306, user = sys.argv[1],
+                            passwd = sys.argv[2], db = sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    sql = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+    cur.execute(sql, (sys.argv[4],))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
