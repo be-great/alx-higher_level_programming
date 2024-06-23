34324f2bbed22b19d5c95816baa86c49913f5dc7
be-great <begreat5333@gmail.com>
2024-06-23 23:03:27 +0300

    hh

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     cmd1 = session.query(State.name, City.id, City.name)
     citys = cmd1.filter(City.state_id == State.id).order_by(City.id).all()
     for city in citys:
-        print(city)
+        print(city[0])
         #print("{}: ({}) {}".format(citys[0], citys[1], citys[2]))
