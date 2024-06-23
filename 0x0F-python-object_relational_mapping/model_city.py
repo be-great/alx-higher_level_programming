f584f39dae7d8599b31acf18e76a0d1b57c5da2e
be-great <begreat5333@gmail.com>
2024-06-23 23:44:42 +0300

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -10,6 +10,6 @@ from model_state import Base
 class City(Base):
     """The state class"""
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
