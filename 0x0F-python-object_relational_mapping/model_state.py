ce35c24b748db5366ecaa481a245e1c130a02603
be-great <begreat5333@gmail.com>
2024-06-23 19:12:23 +0300

    task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+python file that contains the class
+definition of a State and an instance Base
+"""
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, MetaData
-"""python file that contains the class """
-""""definition of a State and an instance Base"""
+
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
