4c633b4c8fa8f16c302ed3b7083e8fd1e10707f7
be-great <begreat5333@gmail.com>
2024-06-23 20:46:59 +0300

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-"""script that lists all states from the database"""
+"""
+Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    sessionmaker_ = sessionmaker(bind=engine)
+    session = sessionmaker_()
+    cmd1 = session.query(State).filter(State.name == sys.argv[4])
+    states = cmd1.first()
+    if states is None:
+        print("Not found")
+    else:
+        print("{}".format(states.id))
