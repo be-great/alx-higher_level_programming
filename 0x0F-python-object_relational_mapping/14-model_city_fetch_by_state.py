79fd7723b257dee72ff1dacee5fb136e0a00be78
be-great <begreat5333@gmail.com>
2024-06-23 23:49:34 +0300

    test task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""
-Start link class to table in database
+""" prints the State object with the name passed as argument from the database
 """
 import sys
+from model_state import Base, State
+from model_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_city import City
-from model_state import Base, State
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
