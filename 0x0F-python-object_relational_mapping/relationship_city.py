c2515d13885b4e2c050ac1b7350bdd640e994db6
be-great <begreat5333@gmail.com>
2024-06-24 03:21:29 +0300

    test

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -5,7 +5,7 @@ definition of a State and an instance Base
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
