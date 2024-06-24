5f3eabd62494acd6f5803d478087b84913e3d15a
be-great <begreat5333@gmail.com>
2024-06-24 03:19:20 +0300

    gf

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,8 +3,8 @@
 Start link class to table in database
 """
 import sys
-from relationship_city import City
 from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
