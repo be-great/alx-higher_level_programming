97ba5f0f62aebd93e0fa3fed7b3e3f57d2e69cbb
be-great <begreat5333@gmail.com>
2024-07-04 19:45:12 +0300

    update task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Function that finds a peak in a list of unsorted integers."""
 
+
 def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers."""
     def binary_search_peak(arr, low, high):
         """Use binary search to find a peak."""
         mid = low + (high - low) // 2
-        
         # Check if the mid  is a peak
         if (mid == 0 or arr[mid] >= arr[mid - 1]) and (
                 mid == len(arr) - 1 or arr[mid] >= arr[mid + 1]):
