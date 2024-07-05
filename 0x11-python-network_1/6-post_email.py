40a7a157784207c148ca213e729c7d5d636b9a4c
be-great <begreat5333@gmail.com>
2024-07-05 17:14:46 +0300

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and
+an email address, sends a POST request
+to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """POST an email #1 """
+    repos = requests.post(sys.argv[1], data= {"email": sys.argv[2]})
+    print(repos.text)
+
