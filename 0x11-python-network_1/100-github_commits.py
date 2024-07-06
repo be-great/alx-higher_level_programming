23897e7ffb83a5e8cc9aa0fad34b8267618666e6
be-great <begreat5333@gmail.com>
2024-07-06 20:17:12 +0300

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
         data = resp.json()
         for i in range(10):
             print(data[i]['parents'][0]['sha'], end=": ")
-            print(data[i]['commit']['committer']['name'])
+            print(data[i]['commit']['author']['name'])
     except Exception as e:
         pass
