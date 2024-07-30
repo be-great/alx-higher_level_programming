3fed169eccd65926b402d0ccc107bfc81b57b7ab
be-great <begreat5333@gmail.com>
2024-07-30 14:29:21 +0300

    update task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+// print the title of episode number of star war
+const request = require('request');
+const movieID = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+function getTitle(movieID) {
+    const fullurl = `${url}${movieID}/`;
+    request.get(url, { json: true}, (error, response, body) => {
+        if (response.statusCode == 200) {
+            console.log(body.title);
+        }
+    });
+}
+
+if (movieID) {
+    getTitle(movieID);
+}
