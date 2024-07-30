ec726d827fe6254ebfa85495fd3207e588c5d46b
be-great <begreat5333@gmail.com>
2024-07-30 15:30:35 +0300

    update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -13,12 +13,12 @@ function printCharacters(movieID) {
                 return;
             }
             function fetchCharacter(index) {
-                if (index > characterUrls.length)
+                if (index >= characterUrls.length)
                     return;
                 const characterUrl = characterUrls[index];
                 request.get(characterUrl, { json: true}, (err0, repos0, body0) => {
                     if (repos0.statusCode == 200) {
-                        console.log(body.name);
+                        console.log(body0.name);
                         fetchCharacter(index + 1);
                     }
                 });
