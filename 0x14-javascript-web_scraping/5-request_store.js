594a4fcdff5f78bff5f527452c685eaae0cfcb81
be-great <begreat5333@gmail.com>
2024-07-30 15:04:03 +0300

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+//  gets the contents of a webpage and stores it in a file.
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filepath = process.argv[3];
+function getContent(url, filepath) {
+    request.get(url, (error, response, body) => {
+        if (response.statusCode == 200) {
+            fs.writeFile(filepath, body, 'utf8', (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            });
+        }
+    });
+}
+
+if (url && filepath) {
+    getContent(url, filepath);
+}
