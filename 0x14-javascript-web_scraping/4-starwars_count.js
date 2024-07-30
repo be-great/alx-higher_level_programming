e7a5232f53bd63323dbd67046bf63130035e57e5
be-great <begreat5333@gmail.com>
2024-07-30 18:53:17 +0300

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,18 +4,12 @@
 const request = require('request');
 const url = process.argv[2];
 const charId = 18;
-function countMoviesWithCharacter (url, charId) {
-  request.get(url, { json: true }, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else if (response.statusCode === 200) {
-      const films = body.results;
-      const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`)).length;
-      console.log(count);
-    }
-  });
-}
-
-if (url) {
-  countMoviesWithCharacter(url, charId);
-}
+request.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const films = body.results;
+    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`)).length;
+    console.log(count);
+  }
+});
