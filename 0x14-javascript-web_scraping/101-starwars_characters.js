d6a08ca60f52a58dc9d595ce98affa7aabcec5be
be-great <begreat5333@gmail.com>
2024-07-30 15:29:23 +0300

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,34 @@
 #!/usr/bin/node
+// print all characters of a star war movie
+// same order of the list “characters”
+const request = require('request');
+const movieID = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+function printCharacters(movieID) {
+    const fullurl = `${url}${movieID}/`;
+    request.get(fullurl, { json: true}, (error, response, body) => {
+        if (response.statusCode == 200) {
+            const characterUrls = body.characters;
+            if (characterUrls.length == 0) {
+                return;
+            }
+            function fetchCharacter(index) {
+                if (index > characterUrls.length)
+                    return;
+                const characterUrl = characterUrls[index];
+                request.get(characterUrl, { json: true}, (err0, repos0, body0) => {
+                    if (repos0.statusCode == 200) {
+                        console.log(body.name);
+                        fetchCharacter(index + 1);
+                    }
+                });
+
+            }
+            fetchCharacter(0);
+        }
+    });
+}
+
+if (movieID) {
+    printCharacters(movieID);
+}
