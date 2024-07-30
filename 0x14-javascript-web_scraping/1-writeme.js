3a48c762add7f78a6cfc8173401877b99a0ebc74
be-great <begreat5333@gmail.com>
2024-07-30 18:48:27 +0300

    ddf

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const filepath = process.argv[2];
 const content = process.argv[3];
-function write (filepath) {
+function write (filepath, content) {
   fs.writeFile(filepath, content, (err) => {
     if (err) {
       console.error(err);
