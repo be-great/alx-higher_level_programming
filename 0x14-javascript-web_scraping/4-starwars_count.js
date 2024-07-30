35ea8a68d7c7b572ac28b17ae542fcbe3ebd8c8e
be-great <begreat5333@gmail.com>
2024-07-30 14:58:22 +0300

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+// print the number of movie of a character present
+// with id = 18
+const request = require('request');
+const url = process.argv[2];
+const charId = 18;
+function countMoviesWithCharacter(url, charId) {
+    request.get(url, { json: true}, (error, response, body) => {
+        if (response.statusCode == 200) {
+            const films = body.results;
+            const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${charId}/`)).length;
+            console.log(count);
+        }
+    });
+}
+
+if (url) {
+    countMoviesWithCharacter(url, charId);
+}
